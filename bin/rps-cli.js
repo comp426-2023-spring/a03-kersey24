0be32b5066394deb6041c89d11d70e4d4929ee53
Ben Kersey <97709332+kersey24@users.noreply.github.com>
2023-04-26 13:11:54 -0400

    formatting error?

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -7,27 +7,25 @@ import minimist from "minimist";
 const args= minimist(process.argv.slice(2));
 
 if (args.h || args.help) {
-    console.log('Usage: node-rps [SHOT]
-Play Rock Paper Scissors (RPS)
-
-  -h, --help      display this help message and exit
-  -r, --rules     display the rules and exit
-
-Examples:
-  node-rps        Return JSON with single player RPS result.
-                  e.g. {"player":"rock"}
-  node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                  e.g {"player":"rock","opponent":"scissors","result":"win"}');
+    console.log("Usage: node-rps [SHOT]");
+    console.log("Play Rock Paper Scissors (RPS)");
+    console.log("\n");
+    console.log("   -h, --help      display this help message and exit");
+    console.log("   -r, --rules     display the rules and exit");
+    console.log("\n");
+    console.log("Examples:");
+    console.log('   node-rps        Return JSON with single player RPS result. e.g. {"player":"rock"}');
+    console.log('   node-rps rock   Return JSON with results for RPS played against a simulated opponent. e.g {"player":"rock","opponent":"scissors","result":"win"}');
     process.exit(0);
 }
 
 if (args.r || args.rules) {
-	console.log('Rules for Rock Paper Scissors:
-
-  - Scissors CUTS Paper
-  - Paper COVERS Rock
-  - Rock CRUSHES Scissors');
-	process.exit(0);
+    console.log("Rules for Rock Paper Scissors:");
+    console.log("\n");
+    console.log("   - Scissors CUTS Paper");
+    console.log("   - Paper COVERS Rock");
+    console.log("   - Rock CRUSHES Scissors");
+    process.exit(0);
 }
 
 if (args._.length === 0){
